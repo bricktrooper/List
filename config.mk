6ac75aa995fa2857ae3b2fcc2d90772b73b7ead3
bricktrooper <kylepinto@yahoo.ca>
2021-12-16 00:07:35 -0500

    Makefiles

---
+++ config.mk
@@ -0,0 +1,39 @@
+# ======= BUILD CONFIG ======= #
+
+# --- COMPILER --- #
+
+CC = clang
+
+# --- LINKER --- #
+
+LD = clang
+
+# --- DEPENDENCY GENERATOR --- #
+
+MM = clang
+
+# --- COMPILER FLAGS --- #
+
+CCFLAGS += -std=c17
+
+# --- LINKER FLAGS --- #
+
+LDFLAGS += -std=c17
+
+# --- LINKER LIBRARIES --- #
+
+LDLIBS +=
+
+# --- ARTIFACTS FOLDER --- #
+
+ART = bin
+
+# --- BINARY EXECUTABLE NAME --- #
+
+EXE = list
+
+# --- SOURCE FILE DIRECTORIES --- #
+
+DIR +=
+
+# ============================ #
